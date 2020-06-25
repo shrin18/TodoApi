713bf5e4a6cd184e4f6cb5a54f128aac98d0d371
Shrinish Donde <shrinish@kth.se>
2020-06-25 17:35:22 +0200

    Followed tutorials !

---
+++ TodoApi/Controllers/SensorsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+
+    public class SensorsController : ControllerBase
+    {
+        public readonly TodoContext _db;
+        public SensorsController(TodoContext database) {
+            _db = database;
+            if (_db.Sensors.Count() == 0) {
+                _db.Sensors.Add(new Sensor
+                {
+                    NodeId = 1,
+                    Type = 1,
+                    Name = "Rainfall",
+                    Description = "Default API server sensor"
+                });
+                _db.SaveChanges();
+            }
+        }
+
+
+
+        // GET: api/<SensorsController>
+        [HttpGet]
+        public ActionResult<List<Sensor>> GetSensors()
+        {
+            return _db.Sensors.ToList();
+        }
+
+        // GET api/<SensorsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Sensor> GetSensor(long id)
+        {
+            var sensor = _db.Sensors.Find(id);
+            if (sensor == null) return NotFound();
+            return sensor;
+        }
+
+        // POST api/<SensorsController>
+        [HttpPost]
+        public ActionResult<Sensor> PostSensor([FromBody] Sensor sensor)
+        {
+            var exists = _db.Sensors.Where(o => o.Name == sensor.Name).Count();
+            if (exists == 0)
+            {
+                if (_db.Nodes.Find(sensor.NodeId) == null) return BadRequest();
+                _db.Sensors.Add(new Sensor
+                {
+                    Name = sensor.Name,
+                    NodeId = sensor.NodeId,
+                    Type = sensor.Type,
+                    Description = sensor.Description
+                });
+                _db.SaveChanges();
+                return _db.Sensors.Last();
+            }
+            else return BadRequest();
+        }
+
+        // PUT api/<SensorsController>/5
+        [HttpPut("{id}")]
+        public ActionResult<Sensor> PutSensor(long id, [FromBody] Sensor update)
+        {
+            var sensor = _db.Sensors.Find(id);
+            if (sensor == null) return NotFound();
+            if (_db.Nodes.Find(update.NodeId) == null) return BadRequest();
+            sensor.NodeId = update.NodeId;
+            sensor.Name = update.Name;
+            sensor.Type = update.Type;
+            sensor.Description = update.Description;
+            _db.SaveChanges();
+            return sensor;
+        }
+        // DELETE api/<SensorsController>/5
+        [HttpDelete("{id}")]
+        public ActionResult<Sensor> DeleteSensor(long id)
+        {
+            var sensor = _db.Sensors.Find(id);
+            if (sensor == null) return NotFound();
+            var deleted = new Sensor
+            {
+                SensorId = 0,
+                NodeId = sensor.NodeId,
+                Type = sensor.Type,
+                Name = sensor.Name,
+                Description = sensor.Description
+            };
+            _db.Remove(sensor);
+            _db.SaveChanges();
+            return deleted;
+        }
+    }
+}
--- TodoApi/Controllers/ValuesController.cs
+++ TodoApi/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +13,86 @@ namespace TodoApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly TodoContext _db;
+        public ValuesController(TodoContext database) {
+            _db = database;
+            if (_db.Nodes.Count() == 0)
+            {
+                _db.Nodes.Add(new Node
+                {
+                    Name = "Server",
+                    Description = "Default API server node"
+                });
+                _db.SaveChanges();
+            }
+
+
+        }
         // GET: api/<ValuesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<List<Node>> GetNodes()
         {
-            return new string[] { "value1", "value2" };
+            return _db.Nodes.ToList();
         }
 
         // GET api/<ValuesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<Node> GetNode(long id)
         {
-            return "value";
+            var node = _db.Nodes.Find(id);
+            if (node == null) return NotFound();
+            return node;
         }
 
         // POST api/<ValuesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Node> PostNode([FromBody] Node node)
         {
+            //var exists = (from x in _db.Nodes where x.Name == node.Name select x.Id).Count();
+            var exists = _db.Nodes.Where(o => o.Name == node.Name).Count();
+            if (exists == 0)
+            {
+                _db.Nodes.Add(new Node
+                {
+                    Name = node.Name,
+                    Description = node.Description
+                });
+                _db.SaveChanges();
+                return _db.Nodes.Last();
+            }
+            else return BadRequest();
         }
 
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<Node> PutNode(long id, [FromBody] Node update)
         {
+            var node = _db.Nodes.Find(id);
+            if (node == null) return NotFound();
+            node.Name = update.Name;
+            node.Description = update.Description;
+            _db.SaveChanges();
+            return node;
         }
 
         // DELETE api/<ValuesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<Node> DeleteNode(long id)
         {
+            var node = _db.Nodes.Find(id);
+            if (node == null) return NotFound();
+            var deleted = new Node
+            {
+                Id = 0,
+                Name = node.Name,
+                Description = node.Description
+            };
+            _db.Remove(node);
+            _db.SaveChanges();
+            return deleted;
         }
+
+
     }
+ 
 }
