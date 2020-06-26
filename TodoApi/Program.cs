88949781fddd297c0b6aea8c482faf0d66cf6225
Shrinish Donde <shdonde@gmail.com>
2020-06-26 19:52:10 +0200

    Add rest calls

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -10,8 +10,8 @@ namespace TodoApi
 
     internal static class Program
     {
-        static string StorageAccountName = "YOURSTORAGEACCOUNTNAME";
-        static string StorageAccountKey = "YOURSTORAGEACCOUNTKEY";
+        static string StorageAccountName = "sigmaiot";
+        static string StorageAccountKey = "3Vupif/boWGadEZ9eJOt7JysQDgxuKp35grvjPw0Vd4=";
 
         private static void Main()
         {
@@ -31,11 +31,23 @@ namespace TodoApi
         /// </summary>
         private static async Task ListContainersAsyncREST(string storageAccountName, string storageAccountKey, CancellationToken cancellationToken)
         {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
 
-            // Construct the URI. This will look like this:
-            //   https://myaccount.blob.core.windows.net/resource
-            String uri = string.Format("http://{0}.blob.core.windows.net?comp=list", storageAccountName);
+            using (var client = new HttpClient(handler))
+            {
+                var response = await client.GetAsync($"https://sigmaiotexercisetest.blob.core.windows.net/api/v1/devices/testdevice/data/2018-09-18/temperature");
 
+                var result = await response.Content.ReadAsStringAsync();
+
+                
+            }
+                // Construct the URI. This will look like this:
+                //   https://myaccount.blob.core.windows.net/resource
+                String uri = string.Format("http://{0}.blob.core.windows.net/api/v1/devices/testdevice/data/2018-09-18/temperature", storageAccountName);
+                String uri_unit = string.Format("http://{0}.blob.core.windows.net/api/v1/devices/testdevice/data/2018-09-18", storageAccountName);
             // Set this to whatever payload you desire. Ours is null because 
             //   we're not passing anything in.
             Byte[] requestPayload = null;
@@ -44,11 +56,10 @@ namespace TodoApi
             using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri)
             { Content = (requestPayload == null) ? null : new ByteArrayContent(requestPayload) })
             {
-
                 // Add the request headers for x-ms-date and x-ms-version.
                 DateTime now = DateTime.UtcNow;
                 httpRequestMessage.Headers.Add("x-ms-date", now.ToString("R", CultureInfo.InvariantCulture));
-                httpRequestMessage.Headers.Add("x-ms-version", "2018-09-18");
+                httpRequestMessage.Headers.Add("x-ms-version", "2017-04-17");
                 // If you need any additional headers, add them here before creating
                 //   the authorization header. 
 
@@ -72,6 +83,37 @@ namespace TodoApi
                     }
                 }
             }
+            using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri_unit)
+            { Content = (requestPayload == null) ? null : new ByteArrayContent(requestPayload) })
+            {
+                // Add the request headers for x-ms-date and x-ms-version.
+                DateTime now = DateTime.UtcNow;
+                httpRequestMessage.Headers.Add("x-ms-date", now.ToString("R", CultureInfo.InvariantCulture));
+                httpRequestMessage.Headers.Add("x-ms-version", "2017-04-17");
+                // If you need any additional headers, add them here before creating
+                //   the authorization header. 
+
+                // Add the authorization header.
+                httpRequestMessage.Headers.Authorization = AzureStorageAuthenticationHelper.GetAuthorizationHeader(
+                   storageAccountName, storageAccountKey, now, httpRequestMessage);
+
+                // Send the request.
+                using (HttpResponseMessage httpResponseMessage = await new HttpClient().SendAsync(httpRequestMessage, cancellationToken))
+                {
+                    // If successful (status code = 200), 
+                    //   parse the XML response for the container names.
+                    if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                    {
+                        String xmlString = await httpResponseMessage.Content.ReadAsStringAsync();
+                        XElement x = XElement.Parse(xmlString);
+                        foreach (XElement container in x.Element("Containers").Elements("Container"))
+                        {
+                            Console.WriteLine("Container name = {0}", container.Element("Name").Value);
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
