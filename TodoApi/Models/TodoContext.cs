cdb3dec9c81ef278d5fd6838d5ad1558c3958fe1
Shrinish Donde <shrinish@kth.se>
2020-06-26 11:31:17 +0200

    Add files for MSVC

--- TodoApi/Models/TodoContext.cs
+++ TodoApi/Models/TodoContext.cs
@@ -4,35 +4,11 @@ namespace TodoApi.Models
 {
     public class TodoContext : DbContext
     {
-        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
         {
         }
-        public DbSet<Node> Nodes { get; set; }
-        public DbSet<Sensor> Sensors { get; set; }
-        public DbSet<SensorValue> SensorValues { get; set; }
-    }
 
-    public class Node
-    {
-        // auto-incremented
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-    public class Sensor
-    {
-        public long SensorId { get; set; }
-        public long NodeId { get; set; }
-        public string Name { get; set; }
-        public int Type { get; set; }
-        public string Description { get; set; }
-    }
-
-    public class SensorValue
-    {
-        public long Id { get; set; }
-        public long SensorId { get; set; }
-        public long Timestamp { get; set; }
-        public string Value { get; set; }
+        public DbSet<TodoItem> TodoItems { get; set; }
     }
 }
--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -1,13 +1,11 @@
-// Unused usings removed
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using TodoApi.Models;
-using System;
-using System.Linq;
 
 namespace TodoApi
 {
@@ -20,21 +18,43 @@ namespace TodoApi
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-            opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("TodoList"));
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
